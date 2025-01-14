1d2d916eb205ee7b7bc8f068537e1deaf4d3d01f
Lina <lina@linacchi.moe>
2025-01-14 20:00:37 +0100

    error handling

--- index.ts
+++ index.ts
@@ -1,9 +1,13 @@
 import express from "express";
 import { env } from "./src/env";
 import { productRouter } from "./app/routes/product";
+import { jsonErrorHandler, notFoundErrorHandler } from "./src/utils/error";
 
 const app = express();
+
 app.use(express.json());
+app.use(jsonErrorHandler);
+app.use(notFoundErrorHandler);
 app.use("/products", productRouter);
 app.listen(env.port, () => {
     console.log("Listening on port", env.port);
--- src/utils/error.ts
+++ src/utils/error.ts
@@ -1,3 +1,4 @@
+import { NextFunction, Request, Response } from "express";
 import { ZodError } from "zod";
 
 export function getErrorMessage(error: ZodError | Error) {
@@ -5,6 +6,23 @@ export function getErrorMessage(error: ZodError | Error) {
         return error.flatten();
     }
     return error.message;
+}
+
+export function jsonErrorHandler(
+    err: Error,
+    _req: Request,
+    res: Response,
+    _next: NextFunction,
+) {
+    res.status(500).json({ error: err });
+}
+
+export function notFoundErrorHandler(
+    _req: Request,
+    res: Response,
+    _next: NextFunction,
+) {
+    res.status(404).json({ error: "Not found" });
 }
 
 function isZodError(error: any): error is ZodError {
